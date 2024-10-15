04633af62d7694043c31efdd71c8fb734c6d68ca
XxELIngexX <cesar.amaya.gomes@gmail.com>
2024-10-15 08:57:15 -0500

    pantalla de inicio con input de nombre

--- src/main/java/edu/eci/arsw/project/flagwarriors/controllers/GameControllers.java
+++ src/main/java/edu/eci/arsw/project/flagwarriors/controllers/GameControllers.java
@@ -3,14 +3,18 @@ package edu.eci.arsw.project.flagwarriors.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 
 @Controller
 public class GameControllers {
-    @GetMapping("/lobby")
-    public String lobby() {
-        return "lobby"; 
+
+    @RequestMapping(value = "/lobby", method = RequestMethod.GET)
+    public String iniciarJuego(@RequestParam String nombre, Model model) {
+        model.addAttribute("nombre", nombre);
+        // Lógica adicional aquí, si es necesario
+        return "lobby"; // Redirige a la vista "lobby"
     }
     @GetMapping("/game")
     public String game() {
