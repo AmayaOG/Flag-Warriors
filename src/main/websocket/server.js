ff559fa81f1eec8d80d38f3fd57f8254e625eb6f
unknown <juan.fernandez-g@labinfo.is.escuelaing.edu.co>
2024-11-06 08:28:55 -0500

    ya hay un servidor y se pueden unir a una sala, pero no se ve reflejado en la pantalla

--- src/main/websocket/server.js
+++ src/main/websocket/server.js
@@ -1,27 +1,39 @@
 // src/main/websocket/server.js
 const WebSocket = require('ws');
+const MAX_PLAYERS_PER_ROOM = 8;
+const rooms = {};
 
 // Crear un servidor WebSocket en el puerto 8080
 const wss = new WebSocket.Server({ port: 8081 });
-
 wss.on('connection', (ws) => {
     console.log('Nuevo jugador conectado');
 
-    // Escuchar mensajes de cada cliente
     ws.on('message', (message) => {
-        console.log(`Mensaje recibido: ${message}`);
+        const data = JSON.parse(message);
+        switch (data.type) {
+            case 'joinRoom':
+                const roomName = data.code;
+                if (!rooms[roomName]) {
+                    rooms[roomName] = {
+                        players: [],
+                    };
+                    console.log(`Se ha creado la sala: ${roomName}`);
+                }
 
-        // Transmitir el mensaje a todos los clientes conectados
-        wss.clients.forEach((client) => {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(message);
-            }
-        });
+                if (rooms[roomName].players.length >= MAX_PLAYERS_PER_ROOM) {
+                    ws.send(JSON.stringify({ type: 'lobbyFull' }));
+                    return;
+                }
+
+                // Agregar jugador a la sala
+                rooms[roomName].players.push({ id: ws.id });
+                console.log(`Jugador ${ws.id} se unió a la sala ${roomName}`);
+                ws.send(JSON.stringify({ type: 'joinedRoom', roomName: roomName }));
+                break;
+        }
     });
 
     ws.on('close', () => {
         console.log('Jugador desconectado');
     });
 });
-
-console.log('Servidor WebSocket en ejecución en ws://localhost:8081');
