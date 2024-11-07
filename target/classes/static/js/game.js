70b144eddf4f7d5b1ae4bf1c766cbca92f2c2edc
Juan Pa fernandez <juanpa20022009@hotmail.com>
2024-11-07 03:50:48 -0500

    agregando target para despliegue

---
+++ target/classes/static/js/game.js
@@ -0,0 +1,48 @@
+
+import { ws, players, lobby } from './lobby.js';
+
+// Espera a que la conexión WebSocket esté lista antes de inicializar la escena
+lobby.connectToWebSocket().then(() => {
+    // Ahora que ws y players están disponibles, puedes inicializar la escena
+    console.log(this.players + "aqui es game");
+    const gameScene = new game(ws, players);
+
+    var config = {
+        type: Phaser.AUTO,
+        scale: {
+            mode: Phaser.Scale.FIT, // escala automáticamente
+            autoCenter: Phaser.Scale.CENTER_BOTH, // centra automáticamente
+            width: 1440, 
+            height: 1080, 
+        },
+        physics: {
+            default: "arcade", // tipo de física
+            arcade: {
+                gravity: { y: 0 }, // la gravedad
+                debug: true // para depurar
+            }
+        },
+        scene: [gameScene]
+    }
+
+    var juego = new Phaser.Game(config);
+}).catch((error) => {
+    console.error('Error al conectar al WebSocket:', error);
+});
+
+
+
+
+const EquipoA = 1; // Define el nombre del equipo azul
+const EquipoB = 2; // Define el nombre del equipo naranja
+
+// Llama a esta función cuando necesites actualizar las puntuaciones
+function actualizarPuntuaciones() {
+    // Obtén las puntuaciones de los equipos usando el método de apiclient
+    const puntuacionA = apiclient.getTeamById().getScore();
+    const puntuacionB = apiclient.getTeamById().getScore();
+
+    // Actualiza el contenido de los párrafos
+    document.getElementById('equipoA').textContent = `Equipo Azul: ${puntuacionA}`;
+    document.getElementById('equipoB').textContent = `Equipo Naranja: ${puntuacionB}`;
+}
---
+++ target/classes/static/js/scene.js
@@ -0,0 +1,247 @@
+class game extends Phaser.Scene {
+    constructor(ws,playersList) {
+        super("gameMap");
+        this.cursors = null;
+        this.currentPlayer = null;
+        this.playerId = null;
+        this.bandera1 = null;
+        this.bandera2 = null;
+        this.playersList = playersList;
+        this.ws=ws;
+        this.getplayer();
+        
+
+    }
+    
+    preload() {
+        console.log(this.playersList)
+        this.load.image("textura", "../map/Textures-16.png");
+        this.load.tilemapTiledJSON("mapa", "../map/mapa.json");
+        this.load.image("banderaAzul", "../images/banderaAzul.png");
+        this.load.image("banderaNaranja", "../images/banderaNaranja.png");
+        this.load.spritesheet("player", this.currentPlayer.path, { frameWidth: 128, frameHeight: 128 });
+
+    }
+    getplayer() {
+        const currentUrl = window.location.href;
+        const url = new URL(currentUrl);
+        const params = new URLSearchParams(url.search);
+        const id = params.get('id');
+
+        this.playersList.forEach(player => {
+            if(player.id == id){
+                this.currentPlayer = player;
+            }
+            
+        });
+    }
+
+    async create() {
+  
+
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        // Animaciones
+        this.anims.create({
+            key: "caminar",
+            frames: this.anims.generateFrameNumbers("player", { start: 1, end: 7 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "quieto",
+            frames: this.anims.generateFrameNumbers("player", { start: 0, end: 0 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        // Mapa
+        var map = this.make.tilemap({ key: "mapa" });
+        var tileset = map.addTilesetImage("muros", "textura");
+        var fondo = map.createLayer("pisosDelJuego", tileset);
+        fondo.setScale(2.25);
+        fondo.setCollisionByProperty({ colision: true });
+
+        // Crear jugador
+
+
+        if (this.currentPlayer.path == "../images/playerA.png") {
+            this.player = this.physics.add.sprite(200, 200, "player");
+            this.player.setCollideWorldBounds(true);
+            this.player.setScale(1);
+            this.player.setSize(30, 80);
+            this.player.setOffset(50, 47);
+        } else {
+            this.player = this.physics.add.sprite(1300, 800, "player");
+            this.player.setCollideWorldBounds(true);
+            this.player.setScale(1);
+            this.player.setSize(30, 80);
+            this.player.setOffset(36, 47);
+        }
+
+
+        // Banderas
+        this.bandera1 = this.physics.add.sprite(1280, 950, 'banderaAzul').setScale(0.3).setSize(100, 100);
+        this.bandera2 = this.physics.add.sprite(180, 120, 'banderaNaranja').setScale(0.3).setSize(100, 100);
+
+        // Colisiones
+        this.physics.add.collider(this.player, fondo);
+
+        if (this.currentPlayer.path == "../images/playerA.png") {
+            this.physics.add.overlap(this.player, this.bandera1, (player, flag) => this.collectFlag(player, flag), null, this);
+        } else {
+            this.physics.add.overlap(this.player, this.bandera2, (player, flag) => this.collectFlag(player, flag), null, this);
+        }
+
+        // Conectar al servidor WebSocket
+        this.connectToWebSocket();
+    }
+
+     connectToWebSocket() {
+        this.ws.onopen = () => {
+            console.log('Conectado al servidor de WebSocket');
+
+            
+        };
+
+        this.ws.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            
+
+            switch (data.type) {
+                case 'startGame':
+                    this.playersList = data.playersList;
+                    console.log("esta es la lista con los jugadores")
+                    console.log(this.playersList)
+                    break;
+                case 'updatePosition':
+                    this.updateOtherPlayerPosition(data);
+                    break;
+                
+                case 'flagCaptured':
+                    actualizarPuntuaciones();
+                    break;
+            }
+        };
+
+        this.ws.onclose = () => {
+            console.log('Desconectado del servidor WebSocket');
+        };
+    }
+    sendStartGameMessage(){
+        console.log(this.ws.readyState);
+        
+        if (this.ws.readyState === WebSocket.OPEN) {
+            const joinMessage = {
+                type: 'startGame',
+            };
+            
+
+            this.ws.send(JSON.stringify(joinMessage));
+            console.log("Mensaje 'startGame' enviado.");
+        } else {
+            console.error("WebSocket no está abierto");
+        }
+        console.log(this.playersList)
+
+    }
+
+
+    update() {
+        if (!this.cursors) return;
+
+        // Movimiento del jugador (esto aún no se actualiza en el servidor)
+        if (this.cursors.right.isDown) {
+            this.player.setVelocityX(150);
+            this.player.anims.play("caminar", true);
+            this.player.flipX = false;
+        } else if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-150);
+            this.player.anims.play("caminar", true);
+            this.player.flipX = true;
+        } else if (this.cursors.up.isDown) {
+            this.player.setVelocityY(-150);
+            this.player.anims.play("caminar", true);
+        } else if (this.cursors.down.isDown) {
+            this.player.setVelocityY(150);
+            this.player.anims.play("caminar", true);
+        } else {
+            this.player.setVelocityX(0);
+            this.player.setVelocityY(0);
+            this.player.anims.play("quieto", true);
+        }
+    }
+
+    updateOtherPlayerPosition(data) {
+        // Aquí puedes implementar la lógica para actualizar la posición de otros jugadores
+        console.log(`Actualizar posición de otro jugador: ID=${data.id}, x=${data.x}, y=${data.y}`);
+    }
+
+    collectFlag(player, flag) {
+        if (!this.player) return;
+        flag.disableBody(true, true);
+        console.log("Player ID:", this.playerId);
+  
+        if (this.playerId) {
+            const flagCaptureMessage = {
+                type: 'flagCaptured',
+                playerId: this.playerId,
+                team: this.currentPlayer.team
+            };
+            this.ws.send(JSON.stringify(flagCaptureMessage));
+  
+            app.captureFlag(this.playerId, function(response) {
+                if (response) {
+                    console.log("Respuesta del servidor:", response);
+                } else {
+                    console.error("No se recibió respuesta del servidor.");
+                }
+            });
+        } else {
+            console.error("ID del jugador no encontrado.");
+        }
+    }
+    renderPlayers() {
+        console.log(this.playersList)
+        this.playersList.forEach(player => {
+            console.log(player)
+            console.log(currentPlayer)
+            if (player.id !== this.currentPlayer.id) {
+                this.renderPlayer(player);
+                
+            }
+
+            
+        });
+    }
+    renderPlayer(player) {
+        const xPosition =player.position.x
+        const yPosition =player.position.y
+        console.log(player)
+
+        this.load.spritesheet("playeradd", player.path, { frameWidth: 128, frameHeight: 128 });
+
+
+        if (player.path == "../images/playerA.png") {
+            this.player = this.physics.add.sprite(xPosition, yPosition, "playeradd");
+            this.player.setCollideWorldBounds(true);
+            this.player.setScale(1);
+            this.player.setSize(30, 80);
+            this.player.setOffset(50, 47);
+        } else {
+            this.player = this.physics.add.sprite(xPosition, yPosition, "playeradd");
+            this.player.setCollideWorldBounds(true);
+            this.player.setScale(1);
+            this.player.setSize(30, 80);
+            this.player.setOffset(36, 47);
+        }
+
+
+        // Lógica para mostrar un jugador en la pantalla
+        console.log(`Renderizando jugador ${player.id}`);
+        // Aquí deberías agregar la lógica gráfica para mostrar al jugador en la posición inicial
+    }
+
+}
+    
