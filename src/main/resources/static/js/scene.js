4be41e5dcec17782515837a30cfa38284d5437c1
AmayaOG <ogiamgo@gmail.com>
2024-11-09 12:58:42 -0500

    Update scene.js

--- src/main/resources/static/js/scene.js
+++ src/main/resources/static/js/scene.js
@@ -70,7 +70,7 @@ class game extends Phaser.Scene {
         const id = params.get('id');
         this.playerId = id;
   
-        this.sceneWs =new WebSocket(`ws://localhost:8081?sessionId=${id}`)
+        this.sceneWs =new WebSocket(`wss://game333-a4994766b26f.herokuapp.com?sessionId=${id}`)
 
         this.sceneWs.onopen = async () => {
             
